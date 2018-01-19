4e0a9b83d34d9463f5e47f5979cac0c402ec0aeb
TrungNguyen1996 <35488158+TrungNguyen1996@users.noreply.github.com>
2018-01-19 10:54:14 +0700

    Update 3Serious exercises 1 n!.py

--- session 2/HomeWork/3Serious exercises 1 n!.py
+++ session 2/HomeWork/3Serious exercises 1 n!.py
@@ -1,6 +1,17 @@
-n = int(input(" 1*2*3*...*n "))
+# n = int(input(" 1*2*3*...*n "))
+#
+# for i in range (0, n-1):
+#     n = n * (i+1)
+#
+# print ("Ket qua '1*2*3*...*n' la: ", n)
 
-for i in range (0, n-1):
-    n = n * (i+1)
+n = int (input("moi ban nhap vao so" ))
 
-print ("Ket qua '1*2*3*...*n' la: ", n)
+sum = 1 # dk dau
+
+for i in range(1, n + 1 ):
+    # print(sum)
+    sum *= i
+print(sum)
+
+# bài anh Huy hướng dẫn chữa 17 01 2018

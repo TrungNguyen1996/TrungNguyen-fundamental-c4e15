3d99162ae6701412351df05d733e73cf220ea5fc
TrungNguyen1996 <35488158+TrungNguyen1996@users.noreply.github.com>
2018-01-19 15:27:04 +0700

    Update Serious exercises 2.py

--- session 3/Homework/Serious exercises 2.py
+++ session 3/Homework/Serious exercises 2.py
@@ -2,4 +2,6 @@
 # TIẾNG VIỆT
 # Sửa đổi mật khẩu người dùng trong phiên trước đó để cho phép người dùng đăng nhập tối đa 3 lần
 
-Hiện e chịu chưa chắc chắn
+Hiện e chịu thôi a, làm cái follow chạc mà ko dc
+Tiếc hôm tối thư 6 a Tuấn anh ko bao e tý nào cả, mât toi lúc ấy rùi
+

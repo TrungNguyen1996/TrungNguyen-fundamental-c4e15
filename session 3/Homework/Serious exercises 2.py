b3d15802249b3a70cac979643bc6246df7a67825
TrungNguyen1996 <35488158+TrungNguyen1996@users.noreply.github.com>
2018-01-19 13:06:21 +0700

    Update Serious exercises 2.py

--- session 3/Homework/Serious exercises 2.py
+++ session 3/Homework/Serious exercises 2.py
@@ -1,3 +1,5 @@
 # Exercise 2: Modify the username password in previous session to allow users login at maximum 3 times
 # TIẾNG VIỆT
 # Sửa đổi mật khẩu người dùng trong phiên trước đó để cho phép người dùng đăng nhập tối đa 3 lần
+
+Hiện e chịu chưa chắc chắn

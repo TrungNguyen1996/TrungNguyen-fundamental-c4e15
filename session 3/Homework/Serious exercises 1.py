3c70347b2bc470ad71ea84987a5345ea033444b2
TrungNguyen1996 <35488158+TrungNguyen1996@users.noreply.github.com>
2018-01-19 13:02:36 +0700

    Update Serious exercises 1.py

--- session 3/Homework/Serious exercises 1.py
+++ session 3/Homework/Serious exercises 1.py
@@ -1,29 +1,41 @@
 # # Exercise 1: Write a program to check whether a number is a prime number
 # TIẾNG VIỆT
 # # Viết một chương trình để kiểm tra xem một số là một số nguyên tố
-num = int(input("Enter a number: "))
-
-
-while True:
-    if num < 2:
-        print("Khong phai so nguyen to")
-    for i in range(2,num + 1):
-        if num == 2:
-            print("So nguyen to")
-        elif num % i == 0:
-            print("Khong phai so nguyen to")
-        else:
-            print("So nguyen to")
+# Kiem tra so dau vao 
+def KierTraDauVao():
+    while Ture:
+    number = int(("nhap sp :"))
+    if number == 2 :
+        print '- So', number,' la so nguyen to!'
         break
-    break
-
-    y = int(input("Enter your number: "))
-x = y // 2
-
-while x > 1:
-    if y % x == 0:
-        print(y, 'is not a Prime number')
+    elif number == 1 :
         break
-    x -= 1
-else:
-    print (y, "is a Prime number")
+    elif number < 0 :
+        print '- so nhap vao phai lon hon 1 va la nguyen duong'
+        print '--- vui long nhap lai so'
+        continue
+    else:
+        break
+    return number
+# Kierm tra so nguyen to
+def Kiermtrasonguyen(number):
+    count = 2
+    while True:
+    if number == 1:
+        print 'so', number, 'khong phai so nguyen to !'
+        break
+
+    number_check = number % count
+
+    if count == number:
+        print 'so', number, ' la so nguyen to !'
+        break
+
+    if number_check  == 0:
+        print 'so', number, 'khong phai so nguyen to !'
+        break
+    else:
+        count = count + 1
+        continue
+
+

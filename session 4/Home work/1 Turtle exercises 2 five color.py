2d0bd9643a284d58d313d22893db1a67d35137ed
TrungNguyen1996 <35488158+TrungNguyen1996@users.noreply.github.com>
2018-01-22 20:37:14 +0700

    Update 1 Turtle exercises 2 five color.py

--- session 4/Home work/1 Turtle exercises 2 five color.py
+++ session 4/Home work/1 Turtle exercises 2 five color.py
@@ -1,1 +1,60 @@
+from turtle import *
+# 1
+begin_fill()
+color("red", "red")
+for i in range(2):
+    forward(50)
+    left(90)
 
+    forward(100)
+    left(90)
+
+forward(50)
+end_fill()
+# 2
+begin_fill()
+color("blue", "blue")
+for i in range(2):
+    forward(50)
+    left(90)
+    forward(100)
+    left(90)
+
+forward(50)
+end_fill()
+# 3+
+begin_fill()
+color("brown", "brown")
+for i in range(2):
+    forward(50)
+    left(90)
+    forward(100)
+    left(90)
+forward(50)
+end_fill()
+# 4
+begin_fill()
+color("yellow", "yellow")
+for i in range(2):
+    forward(50)
+    left(90)
+    forward(100)
+    left(90)
+forward(50)
+end_fill()
+# 5
+begin_fill()
+color("gray", "gray")
+for i in range(2):
+    forward(50)
+    left(90)
+    forward(100)
+    left(90)
+forward(50)
+end_fill()
+
+print("ok")
+mainloop()
+# colors = ["red", "blue", "brown", "yellow", "grey"]
+
+huhu a ơi a dạy e cách nào cho nó ngắn hơn cách này ạ, tối ưu hơn  nhỡ in ra nhiều là ko ổn ạ

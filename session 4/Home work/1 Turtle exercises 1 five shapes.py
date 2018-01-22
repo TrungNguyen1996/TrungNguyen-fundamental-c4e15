09bd4febf5c8b89a69c5335f3db391cec4a8b7fb
TrungNguyen1996 <35488158+TrungNguyen1996@users.noreply.github.com>
2018-01-22 11:25:07 +0700

    Update 1 Turtle exercises 1 five shapes.py

--- session 4/Home work/1 Turtle exercises 1 five shapes.py
+++ session 4/Home work/1 Turtle exercises 1 five shapes.py
@@ -1,3 +1,34 @@
-favs = ["teaching","netflix","death note"]
-print("YOur favorite thing: ")
-print(*favs,sep=", ") #
+from turtle import *
+
+#  Vẽ tam giác 3 cạnh
+color("red")
+for i in range(3):
+    forward(50)
+    left(120)
+
+# Vẽ hình vuông 4 cạnh màu lam
+color("blue")
+for i in range(4):
+    forward(50)
+    left(90)
+
+#  Vé hình ngũ giác 5 cạnh màu nâu
+color("brown")
+for i in range(5):
+    forward(50)
+    left(72)
+
+
+#  Vé hình lục giác 6 cạnh màu vàng
+color("yellow")
+for i in range(6):
+    forward(50)
+    left(60)
+
+#  Vé hình thập giác 6 cạnh màu xám
+color("grey")
+for i in range(7):
+    forward(50)
+    left(51.428571428)
+print("tam giác, vuông, ngũ giác, lục giác, thập giác")
+mainloop()

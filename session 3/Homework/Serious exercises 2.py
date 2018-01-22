19aa20e64f537614480b02a0a13919b5928142f8
TrungNguyen1996 <35488158+TrungNguyen1996@users.noreply.github.com>
2018-01-22 12:03:27 +0700

    Update Serious exercises 2.py

--- session 3/Homework/Serious exercises 2.py
+++ session 3/Homework/Serious exercises 2.py
@@ -3,3 +3,28 @@
 # Sửa đổi mật khẩu người dùng trong phiên trước đó để cho phép người dùng đăng nhập tối đa 3 lần
 
 Hiện e chịu thôi a
+print ("Welcome to Log-in")
+
+n = 3
+p = 3
+while n != 0:
+    username = input("Please input username: ")
+    if username != "c4e":
+        print("Wrong username!")
+        n-=1
+    else:
+        break
+if n == 0:
+    print ("You enter user wrong 3 times. Go away!!")
+else:
+    while p!=0:
+        pass_word = input("Please input password: ")
+        if pass_word != "yay":
+            print("Wrong password!")
+            p-=1
+        else:
+            print ("Yay! You can do anything to me")
+            break
+    else:
+        if p ==0:
+            print ("You enter wrong pass 3 times. Go away!")
